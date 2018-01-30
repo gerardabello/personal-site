bb57b01b4145d48479367e1fba6604dc905bf25d
Gerard Abelló Serras <gerardabello@gmail.com>
2018-01-30 22:29:57 +0100

    Use file loader for projects

--- src/projects.js
+++ src/projects.js
@@ -3,39 +3,46 @@ import styled from 'styled-components'
 
 import { SectionTitle } from './common'
 
+import fldImg from '../assets/imgs/fld.jpg'
+import goobaImg from '../assets/imgs/gooba.jpg'
+import colossalImg from '../assets/imgs/colossal.png'
+import weighImg from '../assets/imgs/weight.png'
+import envelopeImg from '../assets/imgs/envelope-graph.png'
+import gasptImg from '../assets/imgs/gaspt.jpg'
+
 const projects = [
   {
-    img: '/assets/imgs/fld.jpg',
+    img: fldImg,
     href: 'http://upcommons.upc.edu/handle/2099.1/25191?locale-attribute=en',
     title: 'Master thesis',
     description: 'Robot loop detection'
   },
   {
-    img: '/assets/imgs/gooba.jpg',
+    img: goobaImg,
     href: 'http://play.google.com/store/apps/details?id=com.gerardas.gooba',
     title: 'Gooba Monsters',
     description: 'Android puzzle game'
   },
   {
-    img: '/assets/imgs/colossal.png',
+    img: colossalImg,
     href: 'https://colossal.surge.sh',
     title: 'Colossal',
     description: 'A simple poly synth using Web Audio and React'
   },
   {
-    img: '/assets/imgs/weight.png',
+    img: weighImg,
     href: 'https://github.com/gerardabello/weight',
     title: 'Weight',
     description: 'Neural network lib focused on ease of use'
   },
   {
-    img: '/assets/imgs/envelope-graph.png',
+    img: envelopeImg,
     href: 'https://www.npmjs.com/package/adsr-envelope-graph',
     title: 'ADSR Graph',
     description: 'Visualization of an ADSR envelope as a React component'
   },
   {
-    img: '/assets/imgs/gaspt.jpg',
+    img: gasptImg,
     href: 'https://gaspt.serras.xyz',
     title: 'gasPT',
     description: 'Javascript path tracer based on smallPT'
