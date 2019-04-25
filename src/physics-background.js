ddf96f3d3d544c2c4bd40778e772481a5bc6ff23
Gerard Abelló Serras <gerardabello@gmail.com>
2019-04-25 15:45:41 +0200

    refactor: Move physics background to hooks

--- src/physics-background.js
+++ src/physics-background.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Matter from 'matter-js'
 
@@ -13,121 +13,141 @@ const Root = styled.div`
   bottom: 0;
 `
 
-class Background extends React.Component {
-  createEngine(ref) {
-    const width = window.innerWidth
-    const height = window.innerHeight
+const createEngine = element => {
+  const width = window.innerWidth
+  const height = window.innerHeight
 
-    // module aliases
-    const Render = Matter.Render
-    const World = Matter.World
-    const Bodies = Matter.Bodies
+  // module aliases
+  const Render = Matter.Render
+  const World = Matter.World
+  const Bodies = Matter.Bodies
 
-    // create an engine
-    const engine = Matter.Engine.create()
+  // create an engine
+  const engine = Matter.Engine.create()
 
-    // create a renderer
-    const render = Render.create({
-      element: ref,
-      engine: engine,
-      options: {
-        wireframes: false,
-        width,
-        height,
-        background: '#FFFFFF'
+  // create a renderer
+  const render = Render.create({
+    element,
+    engine: engine,
+    options: {
+      wireframes: false,
+      width,
+      height,
+      background: '#FFFFFF'
+    }
+  })
+
+  const friction = 0.1
+
+  const boxColor = '#EEEEEE'
+
+  const shapes = [
+    // create two boxes and a ground
+    Bodies.rectangle(width / 2 - 20, 25, 100, 100, {
+      density: 0.1,
+      friction,
+      render: {
+        fillStyle: boxColor,
+        lineWidth: 0
+      }
+    }),
+    Bodies.rectangle(width / 2 + 20, 120, 80, 80, {
+      density: 0.1,
+      friction,
+      render: {
+        fillStyle: boxColor,
+        lineWidth: 0
+      }
+    }),
+    Bodies.rectangle(width / 2 - 20, 235, 60, 60, {
+      density: 0.1,
+      friction,
+      render: {
+        fillStyle: boxColor,
+        lineWidth: 0
+      }
+    }),
+
+    Bodies.rectangle(width / 2 + 20, 320, 120, 120, {
+      density: 0.1,
+      friction,
+      render: {
+        fillStyle: boxColor,
+        lineWidth: 0
       }
     })
+  ]
 
-    const friction = 0.1
+  const ground = Bodies.rectangle(width / 2, height + 25, width, 50, {
+    isStatic: true,
+    friction
+  })
+  const topGround = Bodies.rectangle(width / 2, -25, width, 50, {
+    isStatic: true,
+    friction
+  })
+  const leftGround = Bodies.rectangle(-25, height / 2, 50, height, {
+    isStatic: true,
+    friction
+  })
+  const rightGround = Bodies.rectangle(width + 25, height / 2, 50, height, {
+    isStatic: true,
+    friction
+  })
 
-    const boxColor = '#EEEEEE'
+  // add all of the bodies to the world
+  World.add(engine.world, [
+    ...shapes,
+    ground,
+    rightGround,
+    topGround,
+    leftGround
+  ])
 
-    const shapes = [
-      // create two boxes and a ground
-      Bodies.rectangle(width / 2 - 20, 25, 100, 100, {
-        density: 0.1,
-        friction,
-        render: {
-          fillStyle: boxColor,
-          lineWidth: 0
-        }
-      }),
-      Bodies.rectangle(width / 2 + 20, 120, 80, 80, {
-        density: 0.1,
-        friction,
-        render: {
-          fillStyle: boxColor,
-          lineWidth: 0
-        }
-      }),
-      Bodies.rectangle(width / 2 - 20, 235, 60, 60, {
-        density: 0.1,
-        friction,
-        render: {
-          fillStyle: boxColor,
-          lineWidth: 0
-        }
-      }),
+  // run the engine
+  Matter.Engine.run(engine)
 
-      Bodies.rectangle(width / 2 + 20, 320, 120, 120, {
-        density: 0.1,
-        friction,
-        render: {
-          fillStyle: boxColor,
-          lineWidth: 0
-        }
-      })
-    ]
+  // run the renderer
+  Render.run(render)
 
-    const ground = Bodies.rectangle(width / 2, height + 25, width, 50, {
-      isStatic: true,
-      friction
-    })
-    const topGround = Bodies.rectangle(width / 2, -25, width, 50, {
-      isStatic: true,
-      friction
-    })
-    const leftGround = Bodies.rectangle(-25, height / 2, 50, height, {
-      isStatic: true,
-      friction
-    })
-    const rightGround = Bodies.rectangle(width + 25, height / 2, 50, height, {
-      isStatic: true,
-      friction
-    })
+  return engine
+}
 
-    // add all of the bodies to the world
-    World.add(engine.world, [
-      ...shapes,
-      ground,
-      rightGround,
-      topGround,
-      leftGround
-    ])
+const useGravity = () => {
+  const [gravity, setGravity] = useState([0, 0])
 
-    // run the engine
-    Matter.Engine.run(engine)
+  useEffect(() => {
+    const handleMotion = event => {
+      const y = event.accelerationIncludingGravity.y / 5
+      const x = -event.accelerationIncludingGravity.x / 5
+      setGravity([x, y])
+    }
 
-    // run the renderer
-    Render.run(render)
+    window.addEventListener('devicemotion', handleMotion, false)
+    return () => {
+      window.removeEventListener('devicemotion', handleMotion)
+    }
+  }, [])
 
-    return engine
+  return gravity
+}
+
+const Background = () => {
+  const [gravityX, gravityY] = useGravity()
+  const engineRef = useRef()
+  const rootRef = useRef()
+
+  if (engineRef.current) {
+    engineRef.current.world.gravity.y = gravityY / 5
+    engineRef.current.world.gravity.x = gravityX / 5
   }
 
-  componentDidMount() {
-    this.engine = this.createEngine(this.ref)
+  useEffect(() => {
+    engineRef.current = createEngine(rootRef.current)
+    return () => {}
+  }, [])
 
-    window.addEventListener('devicemotion', this.handleMotion.bind(this), false)
-  }
-
-  handleMotion(event) {
-    this.engine.world.gravity.y = event.accelerationIncludingGravity.y / 5
-    this.engine.world.gravity.x = -event.accelerationIncludingGravity.x / 5
-  }
-
-  render() {
-    return <Root ref={r => (this.ref = r)} />
-  }
+  return <Root ref={rootRef} />
 }
 
 export default Background
