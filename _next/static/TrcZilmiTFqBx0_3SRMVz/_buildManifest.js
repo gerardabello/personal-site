9ab42f9a6f7458dc9b19c96bf1b6afb9bed2f7c9
gerardabello <gerardabello@users.noreply.github.com>
2022-10-12 08:16:37 +0000

    deploy: 9b4f9dfba22b04d27f39ffa7b7d520c11d1bd9a1

---
+++ _next/static/TrcZilmiTFqBx0_3SRMVz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/793-576ba81c59576b37.js","static/chunks/pages/index-a6e073d3363fe35c.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
