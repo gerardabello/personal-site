86517ea0e61285607b165c7d23d4194d4f3cee08
gerardabello <gerardabello@users.noreply.github.com>
2023-10-26 13:12:50 +0000

    deploy: 14ba5a99d8a2efcffff52292dcb24b11a2ce0679

--- _next/static/pmE35iBpV48X_ld6EMzXm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/793-10e46d0a5fe9d3db.js","static/chunks/pages/index-89a0b7c4b46e6905.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/sccjYMS2x7WydT6xluPN9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/793-10e46d0a5fe9d3db.js","static/chunks/pages/index-88e1083bd3d7f6d7.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
